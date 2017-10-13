f23f695505f658d5da8854df468c4295c1beb512
Thomas Dean <tommydean_sd@yahoo.com>
2017-10-12 19:20:22 -0700

    Create bare source tree with stub main and test files.

---
+++ src/main/java/me/tomdean/gits/GitsApplication.java
@@ -0,0 +1,12 @@
+package me.tomdean.gits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitsApplication.class, args);
+	}
+}
