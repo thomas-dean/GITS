725c2db81be6528e5ec04363d7368e7b48b5cfb2
Thomas Dean <tommydean_sd@yahoo.com>
2017-11-08 12:41:24 -0800

    Add unique constraint to user_name field

--- src/main/resources/db/tables/user.sql
+++ src/main/resources/db/tables/user.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS gits.user (
   id              BIGINT        PRIMARY KEY,
-  user_name       TEXT          NOT NULL,
+  user_name       TEXT          UNIQUE NOT NULL,
   employee_number INTEGER       NOT NULL,
-  password        TEXT          NOT NULL,
+  password        TEXT          NOT NULL
 );
