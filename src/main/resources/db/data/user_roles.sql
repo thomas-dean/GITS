545cec0af051c1458850ae68a625afcd11e696cd
Thomas Dean <tommydean_sd@yahoo.com>
2017-11-10 15:07:30 -0800

    Add missing SQL data scripts

---
+++ src/main/resources/db/data/user_roles.sql
@@ -0,0 +1,4 @@
+INSERT INTO gits.user_roles (user_id, roles_id) VALUES
+  (1, 1),
+  (1, 2),
+  (1, 3);
--- src/main/resources/db/data/users.sql
+++ src/main/resources/db/data/users.sql
@@ -1,3 +1,3 @@
-INSERT INTO gits."user" (id, user_name, employee_number, password) VALUES
+INSERT INTO gits."user" (id, user_name, job_title, employee_number, password) VALUES
   -- Bootstrapping user with password "password"
-  (1, 'admin', 1, '$2a$10$H.zA26Z3SHu0mh6MmQFR0.UcycJGnAE4ym9z/DCezp.cQSCuDp786');
+  (1, 'admin', 'Administrator', 1, '$2a$10$H.zA26Z3SHu0mh6MmQFR0.UcycJGnAE4ym9z/DCezp.cQSCuDp786');
