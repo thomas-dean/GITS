ef0355db5f7c4e027f1c354808e9e1e877474c4a
Thomas Dean <tommydean_sd@yahoo.com>
2017-11-13 18:40:56 -0800

    Remove image -> incident mapping in database

--- src/main/resources/db/tables/image.sql
+++ src/main/resources/db/tables/image.sql
@@ -2,6 +2,5 @@ CREATE SEQUENCE IF NOT EXISTS gits.image_id_seq;
 
 CREATE TABLE IF NOT EXISTS gits.image (
   id              BIGINT        PRIMARY KEY DEFAULT nextval('gits.image_id_seq'::regclass),
-  data            BYTEA         NOT NULL,
-  incident_id     BIGINT        REFERENCES gits.incident(id)
+  data            BYTEA         NOT NULL
 );
